8009364a2c5dfc60847af3b60aabd173c90043cd
Artem Starostenko <artemstarostenko65@gmail.com>
2017-06-28 12:09:56 +0300

    Add script for configuring repos' integration with amazon sns

---
+++ create_hook.py
@@ -0,0 +1,43 @@
+import json
+import requests
+import sys
+
+# depends on the current state of API
+MEDIA_TYPE = "application/vnd.github.inertia-preview+json"
+
+TOKEN = "" # Github token
+ORG_NAME = "mycompany" # organization name on Github
+
+## SNS integration configuration
+AWS_KEY = ""
+AWS_SECRET = ""
+SNS_TOPIC = "" # this should be ARN of a topic
+SNS_REGION = "us-east-1" # region where sns topic was created
+# see all possible types of events here (https://api.github.com/hooks), look for amazonsns
+EVENTS = ["issues", "pull_request"]
+
+headers = {
+    'Accept': MEDIA_TYPE,
+    'Authorization': 'token {}'.format(TOKEN)
+}
+
+data = {
+    'name': 'amazonsns',
+    'config': {
+        'aws_key': AWS_KEY,
+        'sns_topic': SNS_TOPIC,
+        'sns_region': SNS_REGION,
+        'aws_secret': AWS_SECRET
+    },
+    'events': EVENTS,
+    'active': True
+}
+
+def create_hook(*args):
+    for repo in args:
+        url = "https://api.github.com/repos/{0}/{1}/hooks".format(ORG_NAME, repo)
+        r = requests.post(url, headers=headers, data=json.dumps(data))
+        r.raise_for_status()
+        print("Repo name: {0}, Status code: {1}".format(repo,r.status_code))
+
+create_hook(*sys.argv[1:])
